66a99e21bbb9a8921daebd61d62606878fe866d2
Nikolai Knopp <mike_rofone@imail.de>
2025-01-30 00:27:22 +0100

    Convert bot to draw to Display of Roku SoundBridge M1001. Replace polling with callbacks and reset listener when any connection dies.

--- bot.py
+++ bot.py
@@ -1,28 +1,140 @@
-from slackclient import SlackClient
+import logging
 import json
 import os
+import socket
 
-ICON = ":guitar:"
+SOUNDBRIDGE_CONNECT_TIMEOUT_SEC = 5
+
+ICON_X_LEFT = 262
 
 class Bot(object):
-    def __init__(self):
-        self._client = SlackClient(self._token())
-        self._icon = ICON
-        self._client.rtm_connect()
+    def __init__(self, soundbridge_address):
+        self._sock = None
+        socket.setdefaulttimeout(SOUNDBRIDGE_CONNECT_TIMEOUT_SEC)
+        self._soundbridge_address = soundbridge_address
+        self._soundbridge_inited = False
 
-    def _token(self):
-        return os.environ['SLACKBOT_TOKEN']
-    
-    def _channel(self):
-        return os.environ.get('CHANNEL', 'musicreactions')
+    def connectSoundbridge(self):
+        if not self._sock:
+            self._soundbridge_inited = False
+            logging.info(f'Connecting to Soundbridge at {self._soundbridge_address}...')
+            try:
+                self._sock = socket.create_connection((self._soundbridge_address, 4444))
+            except Exception as e:
+                logging.error('Failed to connect within %s seconds: %s', SOUNDBRIDGE_CONNECT_TIMEOUT_SEC, e)
+                return False
+            logging.info('Connected with socket timeout of %s', self._sock.gettimeout())
+        if not self._soundbridge_inited:
+            self.initSoundbridge()
+        return True
 
-    def _username(self):
-        return os.environ.get('SLACK_USERNAME', 'Discobear')
+    def disconnectSoundbridge(self):
+        if not self._sock:
+            logging.info('Already disconnected')
+            return
 
-    def say(self, message):
-        self._client.rtm_send_message(self._channel, message)
+        try:
+            self._sock.shutdown(socket.SHUT_RDWR)
+            self._sock.close()
+        except Exception as e:
+            logging.error('Failed to close connection to Soundbridge: %s', e)
+        finally:
+            del self._sock
+            self._sock = None
+            self._soundbridge_inited = False
+        logging.info('Disconnected from Soundbridge.')
 
-    def sayEx(self, text, image, footer):
-        d = {'thumb_url': image, 'text': text, "footer": footer, "fallback": text}
-        self._client.api_call("chat.postMessage", channel=self._channel(), username=self._username(), as_user="false", icon_emoji=self._icon, attachments=json.dumps([d]))
+    def sendCommandsToSoundbridge(self, commands):
+        if isinstance(commands, str):
+            commands = [commands]
+        try:
+            for command in commands:
+                self._sock.sendall(command + b'\n')
+        except Exception as e:
+            logging.error('Failed to send command to Soundbridge, disconnecting: %s', e)
+            self.disconnectSoundbridge()
+            return False
+        return True
+
+    def initSoundbridge(self):
+        self._soundbridge_inited = self.sendCommandsToSoundbridge([
+            b'sketch',
+            b'encoding utf8',
+            b'clear',
+        ])
+
+    def displayStop(self):
+        self.sendCommandsToSoundbridge([
+            b'color 0',
+            f'rect {ICON_X_LEFT - 1} 0 9 8'.encode(),
+            b'color 1',
+            f'rect {ICON_X_LEFT} 1 6 6'.encode(),
+        ])
+
+    def displayPause(self):
+        self.sendCommandsToSoundbridge([
+            b'color 0',
+            f'rect {ICON_X_LEFT - 1} 0 9 8'.encode(),
+            b'color 1',
+            f'rect {ICON_X_LEFT} 1 2 6'.encode(),
+            f'rect {ICON_X_LEFT + 4} 1 2 6'.encode(),
+        ])
+
+    def displayPlay(self):
+        self.sendCommandsToSoundbridge([
+            b'color 0',
+            f'rect {ICON_X_LEFT - 1} 0 9 8'.encode(),
+            b'color 1',
+            f'rect {ICON_X_LEFT} 0 2 7'.encode(),
+            f'rect {ICON_X_LEFT + 2} 1 2 5'.encode(),
+            f'rect {ICON_X_LEFT + 4} 2 2 3'.encode(),
+            f'point {ICON_X_LEFT + 6} 3'.encode(),
+        ])
+
+    def displayBuffering(self):
+        self.sendCommandsToSoundbridge([
+            b'color 0',
+            f'rect {ICON_X_LEFT - 1} 0 9 8'.encode(),
+            b'color 1',
+            f'line {ICON_X_LEFT} 0 {ICON_X_LEFT + 8} 0'.encode(),
+            f'line {ICON_X_LEFT} 7 {ICON_X_LEFT + 8} 7'.encode(),
+            f'line {ICON_X_LEFT} 1 {ICON_X_LEFT + 3} 4'.encode(),
+            f'line {ICON_X_LEFT} 6 {ICON_X_LEFT + 3} 3'.encode(),
+            f'line {ICON_X_LEFT + 7} 1 {ICON_X_LEFT + 4} 4'.encode(),
+            f'line {ICON_X_LEFT + 7} 6 {ICON_X_LEFT + 4} 3'.encode(),
+        ])
+        
+    def displaySongInfo(self, title, artist, album, length_sec, cast_name):
+        # Ensure connected and initialized, no-op if called repeatedly.
+        self.connectSoundbridge()
+
+        if not title and not artist and not album:
+            # Playing but no metadata happens on first play after starting cast.
+            first_line = f"{cast_name} is starting playback..."
+            second_line = ""
+        else:
+            # Regular playback update
+            title = title or "<Unknown title>"
+            artist = artist or "<Unknown artist>"
+            album = album or "<Unknown album>"
+            first_line = title
+            second_line = f'{artist} | {album}'
+        first_line = first_line.replace("\\", "\\\\").replace("\"", "\\\"")
+        second_line = second_line.replace("\\", "\\\\").replace("\"", "\\\"")
+        minutes = int(length_sec / 60)
+        seconds = int(length_sec % 60)
+        # Manually right-align.
+        duration_x_start = 251 if minutes > 9 else 257
+        status_border_width = 35 if minutes > 9 else 32
+        
+        self.sendCommandsToSoundbridge([
+            b'clear',
+            f'text c 0 "{first_line}"'.encode(),
+            f'text c 8 "{second_line}"'.encode(),
+            b'color 0',
+            f'rect {280 - status_border_width} 0 {status_border_width} 16'.encode(),
+            b'color 1',
+            f'text {duration_x_start} 8 "{minutes:d}:{seconds:02d}"'.encode(),
+        ])
+        logging.info(f'DRAW ON SOUNDBRIDGE FOR {cast_name}:\n  {first_line}\n  {second_line}')
 
--- listener.py
+++ listener.py
@@ -1,15 +1,24 @@
 #!/usr/local/bin/python3.5
 
+# Adapted from https://github.com/sh0oki/chromecastslack/tree/master
+## Useful references
+# https://github.com/home-assistant-libs/pychromecast/blob/master/pychromecast/__init__.py
+# https://github.com/home-assistant-libs/pychromecast/blob/master/pychromecast/controllers/media.py
+# https://github.com/home-assistant-libs/pychromecast/blob/master/pychromecast/controllers/receiver.py
+
 from time import sleep
 from multiprocessing import Lock
 import logging
+import os
 
 import pychromecast
+from pychromecast.controllers.media import MediaStatusListener, MEDIA_PLAYER_STATE_BUFFERING, MEDIA_PLAYER_STATE_PLAYING
 import bot
 
-POLL_INTERVAL = 1800
+HEALTH_CHECK_INTERVAL = 60
 
-class ChromecastListener(object):
+
+class MediaUpdatesListener(MediaStatusListener):
     def __init__(self, player, bot):
         self._song = None
         self._player = player
@@ -17,74 +26,149 @@ class ChromecastListener(object):
         self._lock = Lock()
 
     def new_media_status(self, status):
-        logging.debug("[%s] Got new_media_status %s" % (self._player, status.player_state))
-        if status.player_state != 'PLAYING':
-            logging.debug("[%s] Skipping due to status" % (self._player,))
+        logging.info("[%s] Got new_media_status %s" % (self._player, status.player_state))
+        if not status.player_is_playing and not status.player_is_paused and not status.player_is_idle:
+            logging.info("[%s] Became inactive (%s), releasing Soundbridge" % (self._player, status.player_state))
+            self._bot.disconnectSoundbridge()
             return
-        
-        song = status.media_metadata.get('songName', status.media_metadata['title'])
+
+        # TODO: 
+        # Song length
+        # indicate CC name?
+        title = status.title
+        artist = status.artist
+        album = status.album_name
+        duration = status.duration
+        song = f'{title} - {artist} - {album} - {duration}'
         self._lock.acquire(True)
         try:
-            if song == self._song:
-                logging.debug("[%s] Skipping due to same song again (%s)" % (self._player, self._song))
-                return
-            self._song = song
+            if song != self._song:
+                self._song = song
+                logging.info("New song: %s" % (self._song, ))
+                self.postSong(title, artist, album, duration)
         finally:
             self._lock.release()
+       
+        if status.player_state == MEDIA_PLAYER_STATE_PLAYING:
+            self._bot.displayPlay()
+        elif status.player_state == MEDIA_PLAYER_STATE_BUFFERING:
+            self._bot.displayBuffering()
+        elif status.player_is_paused:
+            self._bot.displayPause()
+        else:
+            self._bot.displayStop()
+    
+    def load_media_failed(self, queue_item_id: int, error_code: int) -> None:
+        """Called when load media failed."""
+        pass # Ignore events.
 
-        logging.info("Posting song %s" % (self._song, ))
-        artist = "?"
-        try:
-            artist = status.media_metadata['artist']
-        except:
-            logging.exception("Failed to get artist")
-            
-        image = ""
-        try:
-            image = status.media_metadata['images'][0]['url']
-        except:
-            logging.exception("Failed to get image")
-        try:
-            self.postSong(artist, song, image)
-        except Exception as e:
-            logging.exception("Failed to post song")
-
-    def postSong(self, artist, song_name, image=None):
-        logging.info("[%s]\t%s - %s (%s)" % (self._player, artist, song_name, image))
-        self._bot.sayEx("%s - %s" % (song_name, artist), image, self._player)
-
-def active_devices():
-    casts, _browser = pychromecast.get_chromecasts()
-    return casts
+    def postSong(self, title, artist, album, length_sec):
+        logging.info(f'[{self._player}] Updating display: {title} - {artist} - {album})')
+        self._bot.displaySongInfo(title, artist, album, length_sec, self._player)
 
 class ChromecastManager(object):
-    def __init__(self, bot):
+    def __init__(self, bot, cast_filter):
         self.active_list = {}
         self.bot = bot
+        self.cast_filter = cast_filter
+        self._lock = Lock()
+        self._browser = None
 
-    def poll(self):
-        for chromecast in active_devices(): 
-            if chromecast.uuid in self.active_list:
-                continue
-            self.register(chromecast)
+    def listenForChromecasts(self):
+        # Stop and delete all listeners, if any.
+        self._lock.acquire(True)
+        try:
+            uuids = list(self.active_list.keys())
+            for uuid in uuids:
+                cast = self.active_list[uuid]
+                try:
+                    cast.disconnect()
+                    del cast
+                except Exception as e:
+                    log.error("Error while disconnecting Chromecast listener: %s", e)
+                del self.active_list[uuid]
+        finally:
+            self._lock.release()
+        if self._browser:
+            self._browser.stop_discovery()
+        # TODO set to 2-5 min
+        self._browser = pychromecast.get_chromecasts(tries=1, timeout=5, retry_wait=10, blocking=False, callback=self.discoveryCallback)
 
-    def register(self, cs):
-        chromecast = cs.device.friendly_name
-        l = ChromecastListener(chromecast, self.bot)
-        if cs is None:
-            logging.error("[%s] Registration failed" % (chromecast, ))
+    def healthCheck(self, uuid):
+        if uuid not in self.active_list:
+            return True
+        cast = self.active_list[uuid]
+        alive = cast.socket_client.is_alive()
+        logging.info(f'Health check on Chromecast {cast.name} with UUID {uuid}: alive=={alive}.')
+        if not alive:
+            logging.warning(f'Chromecast {cast.name} with UUID {uuid} failed health check.')
+        return alive
+
+    def discoveryCallback(self, chromecast):
+        logging.info(f'Discovery of {chromecast.name}')
+        if self.cast_filter and chromecast.name not in self.cast_filter:
+            logging.info(f'Ignoring discovered Chromecast {chromecast.name} due to filter list {self.cast_filter}')
             return
-        cs.wait()
-        mc = cs.media_controller
-        mc.register_status_listener(l)
-        logging.info("[%s] Registered" % (chromecast, ))
-        self.active_list[cs.uuid] = [l, mc]
+            
+        if chromecast.uuid in self.active_list:
+            if self.healthCheck(chromecast.uuid):
+                # Skip known and alive entry.
+                return
+            # Remove unhealthy entry.
+            self._lock.acquire(True)
+            try:
+                cast = self.active_list[chromecast.uuid]
+                cast.disconnect()
+                del self.active_list[chromecast.uuid]
+            finally:
+                self._lock.release()
+        self.register(chromecast)
+
+    # def poll(self):
+        # casts, browser = pychromecast.get_chromecasts(tries=1, timeout=5)
+        # for chromecast in casts:
+            # if chromecast.uuid in self.active_list or (self.cast_filter and chromecast.name not in self.cast_filter):
+                # continue
+            # self.register(chromecast)
+        # # browser.stop_discovery() # This triggers an infinite failure loop upon connection loss, https://github.com/home-assistant-libs/pychromecast/issues/866
+
+    def register(self, cast):
+        if cast is None:
+            logging.error("Registration failed [%s]" % (cs, ))
+            return
+        cc_name = cast.name
+
+        media_listener = MediaUpdatesListener(cc_name, self.bot)
+        self._lock.acquire(True)
+        try:
+            cast.wait()
+            self.active_list[cast.uuid] = cast
+        finally:
+            self._lock.release()
+        cast.media_controller.register_status_listener(media_listener)
+        logging.info("[%s] Registered" % (cc_name, ))
+
 
 def main():
-    m = ChromecastManager(bot.Bot())
+    soundbridge_address = os.environ['SOUNDBRIDGE_IP']
+    if not soundbridge_address:
+        logging.fatal("IP address or name of Soundbridge needs to be specified in SOUNDBRIDGE_IP environment variable")
+    cast_filter = os.environ['CHROMECAST_FILTER'].split(',')
+    if cast_filter:
+        logging.info(f'Only connecting to Chromecasts named {cast_filter}')
+    else:
+        logging.info(f'Will report status of all Chromecasts, use comma-sep values for CHROMECAST_FILTER environment variable to limit')
+
+    m = ChromecastManager(bot.Bot(soundbridge_address), cast_filter)
+    m.listenForChromecasts()
     while True:
-        m.poll()
-        sleep(POLL_INTERVAL)
+        logging.debug("Main loop firing")
+        for uuid in list(m.active_list.keys()):
+            if not m.healthCheck(uuid):
+                # Drop all current connections and reinitalize to ensure healthy state.
+                m.listenForChromecasts()
+        sleep(HEALTH_CHECK_INTERVAL)
+
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
